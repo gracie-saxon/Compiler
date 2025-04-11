ade0e6e57df12a498e83a45e1bcf9e9989e83bee
gracie-saxon <aurora.nebulous@gmail.com>
2025-04-10 22:29:48 -0400

    Update symbols.h

--- symbols.h
+++ symbols.h
@@ -4,32 +4,39 @@
 // Summer 2023
 
 // This file contains the template symbol table
+#ifndef SYMBOLS_H
+#define SYMBOLS_H
+
+#include <map>
+#include <string>
 
 template <typename T>
 class Symbols
 {
 public:
-	void insert(char* lexeme, T entry);
-	bool find(char* lexeme, T& entry);
+    void insert(char* lexeme, T entry);
+    bool find(char* lexeme, T& entry);
 private:
-	map<string, T> symbols;
+    std::map<std::string, T> symbols;
 };
 
 template <typename T>
 void Symbols<T>::insert(char* lexeme, T entry)
 {
-	string name(lexeme);
-	symbols[name] = entry;
+    std::string name(lexeme);
+    symbols[name] = entry;
 }
 
 template <typename T>
 bool Symbols<T>::find(char* lexeme, T& entry)
 {
-	string name(lexeme);
-	typedef typename map<string, T>::iterator Iterator;
-	Iterator iterator = symbols.find(name);
-	bool found = iterator != symbols.end();
-	if (found)
-		entry = iterator->second;
-	return found;
+    std::string name(lexeme);
+    typedef typename std::map<std::string, T>::iterator Iterator;
+    Iterator iterator = symbols.find(name);
+    bool found = iterator != symbols.end();
+    if (found)
+        entry = iterator->second;
+    return found;
 }
+
+#endif
