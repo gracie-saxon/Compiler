1649222e8b905b80188eff6c6ba400aea4d5898c
gracie-saxon <aurora.nebulous@gmail.com>
2025-04-13 16:02:17 -0400

    Update values.h

--- values.h
+++ values.h
@@ -1,15 +1,33 @@
+
+// values.h
 // CMSC 430 Compiler Theory and Design
-// Project 3 Skeleton
-// UMGC CITE
-// Summer 2023
+// Project 3
+// Gracie Saxon
+// April 13, 2025
 
 // This file contains type definitions and the function
 // definitions for the evaluation functions
 
+#ifndef VALUES_H
+#define VALUES_H
+
+#include <vector>
+using namespace std;
+
 typedef char* CharPtr;
 
-enum Operators {ADD, MULTIPLY, LESS, AND};
+enum Operators {
+    // Arithmetic operators
+    ADD, SUBTRACT, MULTIPLY, DIVIDE, REMAINDER, EXPONENT, NEGATION,
+    // Relational operators
+    LESS, GREATER, EQUAL, LESS_EQUAL, GREATER_EQUAL, NOT_EQUAL,
+    // Logical operators
+    AND, OR, NOT,
+    // Direction for fold (not actually an operator)
+    LEFT, RIGHT
+};
 
 double evaluateArithmetic(double left, Operators operator_, double right);
 double evaluateRelational(double left, Operators operator_, double right);
 
+#endif
