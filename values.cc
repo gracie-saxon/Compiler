674afdf720159c1f1da69fa3c3656466e47a506a
gracie-saxon <aurora.nebulous@gmail.com>
2025-04-13 16:02:38 -0400

    Update values.cc

--- values.cc
+++ values.cc
@@ -1,12 +1,14 @@
+// values.cc
 // CMSC 430 Compiler Theory and Design
-// Project 3 Skeleton
-// UMGC CITE
-// Summer 2023
+// Project 3
+// Gracie Saxon
+// April 13, 2025
 
 // This file contains the bodies of the evaluation functions
 
 #include <string>
 #include <cmath>
+#include <limits>
 
 using namespace std;
 
@@ -14,24 +16,66 @@ using namespace std;
 #include "listing.h"
 
 double evaluateArithmetic(double left, Operators operator_, double right) {
-	double result;
-	switch (operator_) {
-		case ADD:
-			result = left + right;
-			break;
-		case MULTIPLY:
-			result = left * right;
-			break;
-	}
-	return result;
+    double result;
+    
+    switch (operator_) {
+        case ADD:
+            result = left + right;
+            break;
+        case SUBTRACT:
+            result = left - right;
+            break;
+        case MULTIPLY:
+            result = left * right;
+            break;
+        case DIVIDE:
+            if (right == 0) {
+                appendError(GENERAL_SEMANTIC, "Division by zero");
+                result = numeric_limits<double>::quiet_NaN();
+            } else {
+                result = left / right;
+            }
+            break;
+        case REMAINDER:
+            if (right == 0) {
+                appendError(GENERAL_SEMANTIC, "Modulo by zero");
+                result = numeric_limits<double>::quiet_NaN();
+            } else {
+                // Use fmod for floating point remainder
+                result = fmod(left, right);
+            }
+            break;
+        case EXPONENT:
+            result = pow(left, right);
+            break;
+    }
+    
+    return result;
 }
 
 double evaluateRelational(double left, Operators operator_, double right) {
-	double result;
-	switch (operator_) {
-		case LESS:
-			result = left < right;
-			break;
-	}
-	return result;
+    double result;
+    
+    switch (operator_) {
+        case LESS:
+            result = left < right;
+            break;
+        case GREATER:
+            result = left > right;
+            break;
+        case EQUAL:
+            result = left == right;
+            break;
+        case LESS_EQUAL:
+            result = left <= right;
+            break;
+        case GREATER_EQUAL:
+            result = left >= right;
+            break;
+        case NOT_EQUAL:
+            result = left != right;
+            break;
+    }
+    
+    return result;
 }
