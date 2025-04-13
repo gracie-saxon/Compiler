1662d67924eff20c569de220b43533aab703b1b0
gracie-saxon <aurora.nebulous@gmail.com>
2025-04-13 15:55:20 -0400

    Update values.cc

--- values.cc
+++ values.cc
@@ -1,37 +1,54 @@
 // CMSC 430 Compiler Theory and Design
-// Project 3 Skeleton
-// UMGC CITE
-// Summer 2023
+// Project 3
+// values.cc
 
 // This file contains the bodies of the evaluation functions
 
 #include <string>
+#include <vector>
 #include <cmath>
 
 using namespace std;
 
 #include "values.h"
-#include "listing.h"
 
-double evaluateArithmetic(double left, Operators operator_, double right) {
-	double result;
-	switch (operator_) {
-		case ADD:
-			result = left + right;
-			break;
-		case MULTIPLY:
-			result = left * right;
-			break;
-	}
-	return result;
+double evaluateReduction(const vector<double>& values, Operators operator_type, bool left)
+{
+    if (values.size() == 0)
+        return 0;
+    
+    if (values.size() == 1)
+        return values[0];
+        
+    double result = values[0];
+    if (left)
+        for (unsigned i = 1; i < values.size(); i++)
+            result = evaluateArithmetic(result, operator_type, values[i]);
+    else
+    {
+        result = values[values.size() - 1];
+        for (int i = values.size() - 2; i >= 0; i--)
+            result = evaluateArithmetic(values[i], operator_type, result);
+    }
+    return result;
 }
 
-double evaluateRelational(double left, Operators operator_, double right) {
-	double result;
-	switch (operator_) {
-		case LESS:
-			result = left < right;
-			break;
-	}
-	return result;
+double evaluateArithmetic(double left, Operators operator_type, double right)
+{
+    switch (operator_type)
+    {
+        case ADD: return left + right;
+        case MULTIPLY: return left * right;
+    }
+    return 0;
+}
+
+double evaluateRelational(double left, Operators operator_type, double right)
+{
+    switch (operator_type)
+    {
+        case LESS: return left < right;
+        case AND: return left && right;
+    }
+    return 0;
 }
