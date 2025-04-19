8242a7e2b614dfc0d767a4deb48368a72c20bf99
gracie-saxon <aurora.nebulous@gmail.com>
2025-04-19 16:39:47 -0400

    Update values.h

--- values.h
+++ values.h
@@ -11,19 +11,13 @@
 
 typedef char* CharPtr;
 
-// Define all operators needed by scanner and parser
 enum Operators {
-    // Arithmetic operators
     ADD, SUBTRACT, MULTIPLY, DIVIDE, REMAINDER, EXPONENT, NEGATION,
-    // Relational operators
     LESS, GREATER, EQUAL, LESS_EQUAL, GREATER_EQUAL, NOT_EQUAL,
-    // Logical operators
     AND, OR, NOT,
-    // Direction indicators (not actual operators)
     LEFT, RIGHT
 };
 
-// Function prototypes
 double evaluateArithmetic(double left, Operators operator_, double right);
 double evaluateRelational(double left, Operators operator_, double right);
 int hexToInt(const char* hexStr);
